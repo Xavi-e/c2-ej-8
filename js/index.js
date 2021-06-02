e5abb49cd67da2d6e23eacabc3f484fded74071b
aleupons <apahoquei@gmail.com>
2021-06-02 13:29:10 +0200

    Clase personaje

--- js/index.js
+++ js/index.js
@@ -1,2 +1,21 @@
 /* eslint-disable max-classes-per-file */
 /* eslint-disable lines-between-class-members */
+class Personaje {
+  nombre;
+  familia;
+  edad;
+  serie;
+  vivo = true;
+  constructor(nombre, familia, edad, serie) {
+    this.nombre = nombre;
+    this.familia = familia;
+    this.edad = edad;
+    this.serie = serie;
+  }
+  comunicar(frase) {
+    return frase;
+  }
+  morir() {
+    this.vivo = false;
+  }
+}
